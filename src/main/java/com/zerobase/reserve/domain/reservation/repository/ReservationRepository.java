3d23abbc1eb60d03f1fe39d0cd37bb2476e6f0a5
do5do <do151015@gmail.com>
2023-11-27 00:16:06 +0900

    예약 확인, 방문 확인 api 작성

--- src/main/java/com/zerobase/reserve/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/zerobase/reserve/domain/reservation/repository/ReservationRepository.java
@@ -1,16 +1,10 @@
 package com.zerobase.reserve.domain.reservation.repository;
 
-import com.zerobase.reserve.domain.member.entity.Member;
 import com.zerobase.reserve.domain.reservation.entity.Reservation;
-import com.zerobase.reserve.domain.reservation.type.ReservationType;
-import com.zerobase.reserve.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>, CustomReservationRepository {
     Optional<Reservation> findByReservationKey(String reservationKey);
-
-    Optional<Reservation> findByMemberAndStoreAndReservationType(
-            Member member, Store store, ReservationType reservationType);
 }
